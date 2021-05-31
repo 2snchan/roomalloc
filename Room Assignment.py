b33438a7085673741e8040c524904cdbbdb44532
2snchan <leesc030720@naver.com>
2021-05-31 14:16:15 +0900

    Edit source code

--- Room Assignment.py
+++ Room Assignment.py
@@ -1,13 +1,13 @@
 # Copyright (c) 2016-2017, 15-021 Kim Shinyeop.
+# Modified by 19-068 Seungchan Lee
 # This file is licensed under a Creative Commons license: CC BY-NC-SA.
-
-ver = '3.0.0'
-version = (3, 0, 0)
 
 import random, csv
 
 def myCmp(x, y):
-    return cmp(x[0], y[0])
+    a = x[0]
+    b = y[0]
+    return (a>b)-(a<b)
 
 def readCsv(filename):
     matrix = []
@@ -18,16 +18,17 @@ def readCsv(filename):
     f.close()
     n = len(matrix)
     if filename == 'team.csv':
-        for i in xrange(n):
+        for i in range(n):
             matrix[i][0] = int(matrix[i][0])
             matrix[i][3] = int(matrix[i][3])
-        matrix.sort(myCmp)
+        matrix.sort()
     elif filename == 'data.csv':
-        for i in xrange(n):
+        for i in range(n):
             matrix[i][0] = int(matrix[i][0])
             matrix[i][1] = int(matrix[i][1])
-        matrix.sort(myCmp)
+        matrix.sort()
     return matrix
+
 
 def convert(roomsM, roomsF):
     rooms = []
@@ -38,10 +39,11 @@ def convert(roomsM, roomsF):
     rooms.sort()
     return rooms
 
+
 def arrangeTickets(rooms, tno, tickets):
     r = len(rooms)
     t = len(tno)
-    ticketsList = [[0]*t for i in xrange(r)]
+    ticketsList = [[0]*t for i in range(r)]
     for ticket in tickets:
         try:
             j = rooms.index(ticket[2])
@@ -51,16 +53,18 @@ def arrangeTickets(rooms, tno, tickets):
             pass
     return ticketsList
 
+
 def countTickets(ticketsList):
     ticketsNum = []
     for ticketList in ticketsList:
         ticketsNum.append(sum(ticketList))
     return ticketsNum
 
+
 def indexMax(nums):
     index = [0]
     maximum = nums[0]
-    for i in xrange(len(nums)):
+    for i in range(len(nums)):
         if nums[i] > maximum:
             maximum = nums[i]
             index = [i]
@@ -69,33 +73,38 @@ def indexMax(nums):
     n = random.randint(0, len(index)-1)
     return index[n]
 
+
 def eraseRow(ticketsList, row):
-    for j in xrange(len(ticketsList[row])):
+    for j in range(len(ticketsList[row])):
         ticketsList[row][j] = 0
 
+
 def eraseCol(ticketsList, col):
-    for i in xrange(len(ticketsList)):
+    for i in range(len(ticketsList)):
         ticketsList[i][col] = 0
+
 
 def findLucky(ticketList):
     ball = []
-    for i in xrange(len(ticketList)):
+    for i in range(len(ticketList)):
         ball.extend([i]*ticketList[i])
     lucky = random.randint(0, len(ball)-1)
     return ball[lucky]
 
+
 def drawCompleted(ticketsList):
-    for i in xrange(len(ticketsList)):
-        for j in xrange(len(ticketsList[0])):
+    for i in range(len(ticketsList)):
+        for j in range(len(ticketsList[0])):
             if ticketsList[i][j] > 0:
                 return False
     return True
+
 
 def drawTickets(ticketsList, rooms, randomAssign):
     r = len(ticketsList)
     t = len(ticketsList[0])
     results = [None] * t
-    remainedRooms = range(r)
+    remainedRooms = list(range(r))
     while not drawCompleted(ticketsList):
         ticketsNum = countTickets(ticketsList)
         room = indexMax(ticketsNum)
@@ -106,7 +115,7 @@ def drawTickets(ticketsList, rooms, randomAssign):
         temp = remainedRooms.index(room)
         del remainedRooms[temp]
     if randomAssign:
-        for i in xrange(len(results)):
+        for i in range(len(results)):
             if results[i] == None:
                 temp = random.randint(0, len(remainedRooms)-1)
                 room = remainedRooms[temp]
@@ -114,20 +123,22 @@ def drawTickets(ticketsList, rooms, randomAssign):
                 del remainedRooms[temp]
     return results
 
+
 def exportResults(results, teams):
     t = len(teams)
     matrix = []
-    for i in xrange(t):
+    for i in range(t):
         matrix.append(teams[i] + [results[i]])
-    f = open('result.csv', 'wb')
+    f = open('result.csv', 'w')
     cw = csv.writer(f, delimiter=',')
-    for i in xrange(len(matrix)):
+    for i in range(len(matrix)):
         cw.writerow(matrix[i])
     f.close()
 
+
 def exportSQL(results, teams):
     f = open('SQL.txt', 'w')
-    for i in xrange(len(teams)):
+    for i in range(len(teams)):
         r = results[i]
         if r != None:
             t1 = teams[i][1]
@@ -142,17 +153,21 @@ def exportSQL(results, teams):
                 f.write("\n")
     f.close()
 
+
 def main():
-    exec open('settings.txt', 'r').read()
+    randomAssign = False
+    roomsM = [103,108,109,110,113,114,118,241,242,309,310,319,323,329,330,342]
+    roomsF = [206,208,211,212,216,308,312]
     rooms = convert(roomsM, roomsF)
     teams = readCsv('team.csv')
-    tno = [teams[i][0] for i in xrange(len(teams))]
+    tno = [teams[i][0] for i in range(len(teams))]
     tickets = readCsv('data.csv')
     ticketsList = arrangeTickets(rooms, tno, tickets)
     results = drawTickets(ticketsList, rooms, randomAssign)
     exportResults(results, teams)
     exportSQL(results, teams)
-    print "Room Assignment Completed!!!"
+    print("Room Assignment Completed!!!")
+
 
 if __name__ == '__main__':
     main()
